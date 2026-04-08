3f7338dd49a8b0bf9fef00d4611c6013af9e65d6
zhangxingbin <xbzhang2020@icloud.com>
2026-04-08 23:00:51 +0800

    chore: remove tkinter

--- gui_tkinter/styling/label.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-label = tk.Label(root, text="Hello, World!", foreground="red", background="blue")
-label.pack()
-
-root.mainloop()
--- gui_tkinter/tk_widgets/label.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-label = tk.Label(root, text="Hello, World!")
-label.pack()
-
-root.mainloop()
