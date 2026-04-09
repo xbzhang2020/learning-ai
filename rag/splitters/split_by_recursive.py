6b70cbe07309a6a62c681ce1f7a40f61b69283fe
zhangxingbin <xbzhang2020@icloud.com>
2026-04-09 23:24:54 +0800

    chore: 代码优化

--- rag/splitters/split_by_recursive.py
+++ rag/splitters/split_by_recursive.py
@@ -4,9 +4,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 loader = TextLoader("data/shanxi/云冈石窟.txt")
 docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100, chunk_overlap=20, separators=["\n\n", "\n", " ", "", "。", "，"]
-)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
 chunks = text_splitter.split_documents(docs)
 
 for chunk in chunks:
