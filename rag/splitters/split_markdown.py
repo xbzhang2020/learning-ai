c9ffe8903d47d5792191e4c3731f8a72b817212f
zhangxingbin <xbzhang2020@icloud.com>
2026-04-11 01:21:56 +0800

    feat: splitters

--- rag/splitters/split_markdown.py
+++ rag/splitters/split_markdown.py
@@ -1,29 +1,37 @@
+"""
+按照 Markdown 标题进行分块
+
+优点：可以按照 Markdown 标题进行分块，适用于 Markdown 文档。
+缺点：只能按照 Markdown 标题进行分块，无法按照其他规则进行分块。
+"""
+
 from langchain_text_splitters import (
     MarkdownHeaderTextSplitter,
     RecursiveCharacterTextSplitter,
 )
 
-with open("assets/wukong/黑悟空版本介绍.md", "r", encoding="utf-8") as f:
-    markdown_text = f.read()
+if __name__ == "__main__":
+   
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+    ]
 
-headers_to_split_on = [
-    ("#", "Header 1"),
-    ("##", "Header 2"),
-    ("###", "Header 3"),
-]
+    markdown_splitter = MarkdownHeaderTextSplitter(
+        headers_to_split_on=headers_to_split_on,
+    )
+   
+    with open("assets/wukong/黑悟空版本介绍.md", "r", encoding="utf-8") as f:
+        markdown_text = f.read()
+    markdown_chunks = markdown_splitter.split_text(markdown_text)
 
-markdown_splitter = MarkdownHeaderTextSplitter(
-    headers_to_split_on=headers_to_split_on,
-)
-markdown_chunks = markdown_splitter.split_text(markdown_text)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20,
+    )
+    chunks = text_splitter.split_documents(markdown_chunks)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=50,
-    chunk_overlap=0,
-)
-chunks = text_splitter.split_documents(markdown_chunks)
-
-for chunk in chunks:
-    print(chunk.metadata)
-    print(chunk.page_content)
-    print("-" * 10)
+    with open("output/wukong_version_markdown_chunks.txt", "w") as f:
+        for chunk in chunks:
+            f.write(f"{str(chunk.metadata)}\n{chunk.page_content}\n\n{"-" * 10}\n\n")
