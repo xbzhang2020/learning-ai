f69ad2a0336bf86b36ac45bde67dbfede9635321
zhangxingbin <xbzhang2020@icloud.com>
2026-04-10 22:20:58 +0800

    chore: split

--- rag/loaders/load_web.py
+++ rag/loaders/load_web.py
@@ -1,13 +1,17 @@
 from langchain_community.document_loaders import WebBaseLoader
 import bs4
 
-loader = WebBaseLoader(
-    web_path="https://zh.wikipedia.org/wiki/%E9%BB%91%E7%A5%9E%E8%AF%9D%EF%BC%9A%E6%82%9F%E7%A9%BA",
-    bs_kwargs={"parse_only": bs4.SoupStrainer(id="bodyContent")},
-)
-docs = loader.load()
+def load_wukong_wiki_docs():
+    loader = WebBaseLoader(
+        web_path="https://zh.wikipedia.org/wiki/%E9%BB%91%E7%A5%9E%E8%AF%9D%EF%BC%9A%E6%82%9F%E7%A9%BA",
+        bs_kwargs={"parse_only": bs4.SoupStrainer(id="bodyContent")},
+    )
+    docs = loader.load()
+    return docs
 
-print(len(docs))
+if __name__ == "__main__":
+    docs = load_wukong_wiki_docs()
+    print(len(docs))
 
-with open("assets/wukong/wukong_wiki.txt", "w") as f:
-    f.write(docs[0].page_content.strip())
+    with open("assets/wukong/wukong_wiki.txt", "w") as f:
+        f.write(docs[0].page_content.strip())
--- rag/splitters/split_by_length.py
+++ rag/splitters/split_by_length.py
@@ -5,18 +5,19 @@
 缺点：尽管可以设置重叠度，但仍然可能会将一句话切分成两半，破坏句子完整性
 """
 
-from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
+from rag.loaders.load_web import load_wukong_wiki_docs
 
-
-if __name__ == "__main__":
-    loader = TextLoader("assets/wukong/wukong_wiki.txt")
-    docs = loader.load()
-
+def split_wukong_wiki_docs_by_length():
+    docs = load_wukong_wiki_docs()
     text_splitters = CharacterTextSplitter(
         separator="", chunk_size=1000, chunk_overlap=20
     )
     chunks = text_splitters.split_documents(docs)
+    return chunks
+
+if __name__ == "__main__":
+    chunks = split_wukong_wiki_docs_by_length()
 
     for chunk in chunks[:5]:
         print(chunk.metadata)
