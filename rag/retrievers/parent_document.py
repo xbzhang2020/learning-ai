b105638591b74f25f9a94128418154fcaabacf0d
zhangxingbin <xbzhang2020@icloud.com>
2026-04-14 16:14:56 +0800

    feat: retrievers

---
+++ rag/retrievers/parent_document.py
@@ -0,0 +1,49 @@
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_classic.storage import InMemoryStore
+from langchain_classic.retrievers import ParentDocumentRetriever
+from langchain_core.documents import Document
+from rag.embeddings.bge_small_zh import embeddings
+
+# 父文本分块器
+parent_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000, 
+    add_start_index=True,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", "。", "！", "？", "；", ",", " ", ""]
+)
+# 子文本分块器
+child_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400, 
+    add_start_index=True,
+    chunk_overlap=50,
+    separators=["\n\n", "\n", "。", "！", "？", "；", ",", " ", ""]
+)
+
+# 创建向量存储，存储子文本
+vectorstore = Chroma(embedding_function=embeddings)
+# 创建存储层，存储父文本
+store = InMemoryStore()
+
+# 创建检索器
+retriever = ParentDocumentRetriever(
+    vectorstore=vectorstore,
+    docstore=store,
+    child_splitter=child_splitter,
+    parent_splitter=parent_splitter,
+)
+
+if __name__ == "__main__":
+    with open("assets/wukong/玩法.txt", "r", encoding="utf-8") as f:
+        game_knowledge = f.read()
+    
+    documents = [Document(page_content=game_knowledge)]
+
+    # 添加文档
+    retriever.add_documents(documents)
+
+    # 检索文档
+    results = retriever.invoke("游戏的玩法")
+    for result in results:
+        print(result.page_content)
+        print("-" * 10)
--- rag/vector_stores/milvus_search.py
+++ rag/vector_stores/milvus_search.py
@@ -1,7 +1,7 @@
-from rag.vector_stores.chroma import chroma_wukong_db
+from rag.vector_stores.milvus import milvus_wukong_db
 
 if __name__ == "__main__":
-    results = chroma_wukong_db.similarity_search(
+    results = milvus_wukong_db.similarity_search(
         "景区门票",
         k=3,
     )
